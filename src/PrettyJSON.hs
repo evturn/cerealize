1ec65433ac3314b1dac308bfb835fe415a6798b5
Evan Turner <ev@evturn.com>
2017-12-06 14:57:48 -0500

    Add functions to operate on bit level data

--- src/PrettyJSON.hs
+++ src/PrettyJSON.hs
@@ -1,16 +1,12 @@
 module PrettyJSON where
 
 import           Cereal
+import           Data.Bits (shiftR, (.&.))
 import           Data.List (intercalate)
+import           Numeric   (showHex)
 
 data Doc = ToBeDefined
   deriving Show
-
-string :: String -> Doc
-string = enclose '"' '"' . hcat . map oneChar
-
-enclose :: Char -> Char -> Doc -> Doc
-enclose l r x = char l <> x <> char r
 
 (<>) :: Doc -> Doc -> Doc
 a <> b = undefined
@@ -26,6 +22,38 @@ text s = undefined
 
 double :: Double -> Doc
 double n = undefined
+
+string :: String -> Doc
+string = enclose '"' '"' . hcat . map oneChar
+
+enclose :: Char -> Char -> Doc -> Doc
+enclose l r x = char l <> x <> char r
+
+oneChar :: Char -> Doc
+oneChar c = case lookup c simpleEscapes of
+              Just r -> text r
+              Nothing | mustEscape c -> hexEscape c
+                      | otherwise    -> char c
+  where
+    mustEscape c = c < ' ' || c == '\x7f' || c > '\xff'
+
+simpleEscapes :: [(Char, String)]
+simpleEscapes = zipWith ch "\b\n\f\r\t\\\"/"
+  where
+    ch a b = (a, ['\\', b])
+
+smallHex :: Int -> Doc
+smallHex x = text "\\u"
+          <> text (replicate (4 - length h) 'o')
+          <> text h
+  where
+    h = showHex x ""
+
+astral :: Int -> Doc
+astral n = smallHex (a + 0xd800) <> smallHex (b + 0xdc00)
+  where
+    a = (n `shiftR` 10) .&. 0x3ff
+    b = n .&. 0x3ff
 
 renderJValue :: JValue -> Doc
 renderJValue (JBool True)  = text "true"
